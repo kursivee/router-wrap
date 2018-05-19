e9353b97387e0ea8feb2adf911c4884caa0012db
spookymon <kursivee@4k.local>
2018-05-19 08:46:05 -0400

    Refactoring

--- src/routerWrap.js
+++ src/routerWrap.js
@@ -1,12 +1,11 @@
 const routerStore = require('./routerStore');
 
 exports.register = function (path, router) {
-    console.log('register');
     routerStore.push(path, router);
 }
 
 exports.connect = function (app) {
     routerStore.getRouters().forEach((router) => {
         app.use(router.path, router.router);
-});
+    });
 }
